9cf838fa5b29c56261fd7b10ff6b57b2fd4a1876
Nico Oracion <nico.oracion.dev@gmail.com>
2022-05-21 20:23:36 +0800

    API: - Add createAccount - Add getAccount

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,11 @@ object Dependencies {
   val AkkaVersion = "2.6.8"
   val AkkaHttpVersion = "10.2.9"
 
-  val AkkaActor = "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion
-  val AkkaStream = "com.typesafe.akka" %% "akka-stream" % AkkaVersion
+  val AkkaActors = "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion
+  val AkkaStreams = "com.typesafe.akka" %% "akka-stream" % AkkaVersion
   val AkkaHttp = "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
+  val AkkaSpray = "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
 
+  val SprayVersion = "1.3.6"
+  val SprayJson = "io.spray" %%  "spray-json" % SprayVersion
 }
--- src/main/scala/bank/Main.scala
+++ src/main/scala/bank/Main.scala
@@ -1,7 +1,16 @@
 package bank
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+
 
 object Main extends App {
 
   println("Welcome to Banking app")
+
+  val controller = new Controller
+
+  implicit val actorSystem = ActorSystem()
+
+  Http().newServerAt("localhost",9000).bind(controller.allRoutes)
 }
