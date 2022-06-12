3757628b7b7d5bb7f7e6b6dadb94c4f2731a771b
Nico Oracion <nico.oracion.dev@gmail.com>
2022-06-12 15:54:33 +0800

    fix createAccount unit test

--- src/test/scala/bank/ControllerSpec.scala
+++ src/test/scala/bank/ControllerSpec.scala
@@ -6,7 +6,8 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.util.ByteString
-class ControllerSpec extends AnyWordSpec with Matchers with ScalatestRouteTest{
+import spray.json.{DefaultJsonProtocol, enrichAny}
+class ControllerSpec extends AnyWordSpec with Matchers with ScalatestRouteTest with SprayJsonSupport with DefaultJsonProtocol{
 
   "Banking app" should{
     val controller = new Controller
@@ -26,6 +27,24 @@ class ControllerSpec extends AnyWordSpec with Matchers with ScalatestRouteTest{
       postRequest ~> controller.createAccount ~> check {
         status shouldEqual StatusCodes.Created
         responseAs[String] shouldEqual "\"Account created\""
+      }
+    }
+
+    "get an account" in {
+      val getRequest = HttpRequest(
+        HttpMethods.GET,
+        uri = "/api/accounts/123"
+      )
+      val account = Account(
+        accountNumber = "123",
+        currentBalance = 9999,
+        currency = "PHP",
+        customerFirstName = "Lloyd",
+        customerLastName = "Edano"
+      )
+      getRequest ~> controller.getAccountByAccountNum ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[String] shouldEqual (s"Account found: ${account.toJson}".toJson).toString
       }
     }
   }
